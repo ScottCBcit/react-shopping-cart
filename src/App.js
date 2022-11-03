31bbb8762c999b5d369e441f36931af6fbe99480
Scott Croin <scroin@my.bcit.ca>
2022-11-03 15:02:47 -0700

    Working shopping cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
   const productsArray = ["Shirt", "Pants", "Tie", "Jacket", "Hat", "Scarf"];
   const productItems = productsArray.map((item, i) => <option key={i} value={item}>{item}</option>);
-  const selectedItem = "";
-  
-  const [item, setItem] = useState("");
+  const [cart, setCart] = useState([])
+  const cartDisplay = cart.map((item, i) => <li key={i}>{item}</li>);
+  const [item, setItem] = useState("Shirt");
 
 
 
@@ -18,11 +18,14 @@ function App() {
         <h1>Shopping Cart</h1>
         <form>
         <label for="products">Choose a Product: </label>
-        <select onChange={e => {setItem = (e.target.value);}} name="products" id="products">{productItems}</select>
-        <button onClick={(event) =>{console.log(item)}} className="addToCart" type="button">Add To Cart</button>
+        <select onChange={e => {setItem(e.target.value);}} name="products" id="products">{productItems}</select>
+        <button onClick={(event) =>{setCart([item].concat(cart))}} className="addToCart" type="button">Add To Cart</button>
+        <button onClick={(event) =>{console.log(cart)}} className="addToCart" type="button">Add To Cart</button>
       </form>
       <div>
-        <p>Test</p>
+        <ul>
+          {cartDisplay}
+        </ul>
       </div>
       </header>
     </div>
